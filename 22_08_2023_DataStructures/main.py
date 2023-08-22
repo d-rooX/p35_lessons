d65558994c0e18d95d0bc471117de8130758b98e
d-rooX <bot0463@gmail.com>
2023-08-22 15:03:57 +0300

    SinglyLinkedList: insert method

--- 22_08_2023_DataStructures/main.py
+++ 22_08_2023_DataStructures/main.py
@@ -3,6 +3,8 @@ class Node:
         self.data = data
         self.next = None
 
+    def __str__(self):
+        return str(self.data)
 
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,50 @@ class SinglyLinkedList:
         else:
             self.head = node
             self.tail = node
+
+    def insert(self, data, index):
+        node = Node(data)
+
+        if index == 0:
+            node.next = self.head
+            self.head = node
+            return
+
+        current = self.head
+        prev = self.head
+        current_index = 0
+
+        while current:
+            if current_index == index:
+                prev.next = node
+                node.next = current
+                return
+
+            prev = current
+            current = current.next
+            current_index += 1
+
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current
+            current = current.next
+
+
+sll = SinglyLinkedList()
+sll.append("1")
+sll.append("2")
+sll.append("3")
+
+for i in sll:
+    print(i)
+
+print('____')
+
+sll.insert("INSERTED", 0)
+
+for i in sll:
+    print(i)
 
 
 
