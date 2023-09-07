dede42a1886dd658a21acff004559f8c43b61274
d-rooX <bot0463@gmail.com>
2023-09-07 20:26:21 +0300

    multithreading client-server chat

--- 07_05_2023_Requests/first_task/client.py
+++ 07_05_2023_Requests/first_task/client.py
@@ -1,18 +1,28 @@
 import socket
+import threading
+
+
+def listen_server(server_socket):
+    while True:
+        data = server_socket.recv(1024)
+        if not data:
+            return
+        print(data.decode('utf-8'))
+
 
 client = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-client.connect(('localhost', 1337))
+client.connect(('localhost', 1339))
 
+# printing welcome message
 print(client.recv(1024).decode('utf-8'))
 
+# listening for server
+t = threading.Thread(target=listen_server, args=(client, ))
+t.start()
+
 while True:
-    message = input('>>> ')
+    message = input()
     client.send(message.encode('utf-8'))
 
-    data = client.recv(1024)
-    print(f'[SERVER] {data.decode("utf-8")}')
-
-
-
-
-
+    # data = client.recv(1024)
+    # print(f'[SERVER] {data.decode("utf-8")}')
--- 07_05_2023_Requests/first_task/server.py
+++ 07_05_2023_Requests/first_task/server.py
@@ -1,15 +1,17 @@
 import socket
+import threading
 import time
 
 server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-server_socket.bind(('localhost', 1337))
+server_socket.bind(('localhost', 1339))
 server_socket.listen(1)
 
-while True:
-    print('Listening for connection...')
-    client_socket, address = server_socket.accept()
-    print(f'Client connected! {address}')
 
+def listen_client(client_socket, address):
+    global client_sockets
+    client_sockets.append(client_socket)
+
+    print(f'Client connected! {address}')
     greeting = f'Welcome in chat!\n[{time.strftime("%H:%M:%S")}]'
     client_socket.send(greeting.encode('utf-8'))
 
@@ -18,9 +20,22 @@ while True:
         if not data:
             break
 
-        print(f'[{address}] {data.decode("utf-8")}')
-        message = input('>>> ')
-        client_socket.send(message.encode('utf-8'))
+        data = f'[{address}] {data.decode("utf-8")}'
+        print(data)
 
+        for guest_socket in client_sockets:
+            if guest_socket is not client_socket:
+                guest_socket.send(data.encode('utf-8'))
+
+    client_sockets.remove(client_socket)
     print(f'Client [{address}] disconnected!')
 
+
+client_sockets = []
+while True:
+    print(f'Connected clients: {len(client_sockets)}')
+    print('Listening for connection...')
+    client_socket, address = server_socket.accept()
+
+    t = threading.Thread(target=listen_client, args=(client_socket, address))
+    t.start()
